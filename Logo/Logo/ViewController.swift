0958147a8dc79d875ebbc7674cdbf76ce1843d95
Jordan Christensen <jordan.c4922@gmail.com>
2019-09-12 02:58:48 +0800

    Add xcode project

---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by Jordan Christensen on 9/12/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
