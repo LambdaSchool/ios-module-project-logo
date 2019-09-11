40cb743bca15727cfd3f077377592ce3144a2a8a
brian vilchez <brianvilchez@brians-MacBook-Pro-2.local>
2019-09-11 14:22:07 -0600

    create xcode project

---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by brian vilchez on 9/11/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
