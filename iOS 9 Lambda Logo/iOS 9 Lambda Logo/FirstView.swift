f69defa165f88486d7f12c16301971a7fa9af6ea
Alex Shillingford <alex.shillingford@icloud.com>
2019-09-11 15:27:56 -0600

    Achieve MVP

--- iOS 9 Lambda Logo/iOS 9 Lambda Logo/FirstView.swift
+++ iOS 9 Lambda Logo/iOS 9 Lambda Logo/FirstView.swift
@@ -10,12 +10,40 @@ import UIKit
 
 class FirstView: UIView {
 
-    /*
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        if let context = UIGraphicsGetCurrentContext() {
+            
+            // Lambda outline
+            context.move(to: CGPoint(x: (rect.width / 2) - 8, y: rect.height - 30))
+            context.addLine(to: CGPoint(x: rect.width * 0.8, y: rect.height * 0.65))
+            context.addLine(to: CGPoint(x: rect.width * 0.8, y: rect.height * 0.2))
+            context.addLine(to: CGPoint(x: rect.width * 0.2, y: rect.height * 0.2))
+            context.addLine(to: CGPoint(x: rect.width * 0.2, y: rect.height * 0.65))
+            context.addLine(to: CGPoint(x: (rect.width / 2) + 8, y: rect.height - 30))
+            context.setStrokeColor(UIColor.red.cgColor)
+            context.setLineWidth(25)
+            context.strokePath()
+            
+            // Lambda symbol
+            
+            context.move(to: CGPoint(x: rect.width / 2, y: rect.height))
+            context.addLines(between: [CGPoint(x: (rect.width / 2), y: rect.height * 0.42),
+                                       CGPoint(x: (rect.width / 2) + 35, y: rect.height * 0.62),
+                                       CGPoint(x: (rect.width / 2) + 60, y: rect.height * 0.62),
+                                       CGPoint(x: (rect.width / 2) + 15, y: rect.height * 0.37),
+                                       CGPoint(x: (rect.width / 2) - 15, y: rect.height * 0.37),
+                                       CGPoint(x: (rect.width / 2) - 60, y: rect.height * 0.62),
+                                       CGPoint(x: (rect.width / 2) - 35, y: rect.height * 0.62)])
+            context.closePath()
+            context.setFillColor(UIColor.red.cgColor)
+            context.setLineWidth(1)
+            context.fillPath()
+
+        }
     }
-    */
+    
 
 }
