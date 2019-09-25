7362245b8ca69cbf7cd12bdd2f665a7893941811
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-25 00:28:19 -0400

    completed the logo! yay!

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -13,6 +13,8 @@ class LogoView: UIView {
     
     private let viewBgColor = UIColor.green
     private let borderRadius = CGFloat(10.0)
+    private let borderRadiusInside = CGFloat(8.0)
+    private let lambdaColor = UIColor(displayP3Red: 234/255, green: 24/255, blue: 64/255, alpha: 1)
 
     override func draw(_ rect: CGRect) {
         let rect = rect.insetBy(dx: 10, dy: 10)
@@ -32,8 +34,8 @@ class LogoView: UIView {
             path.addLine(to: topLeft)
             path.close()
 
-            context.setFillColor(UIColor.red.cgColor)
-            context.setStrokeColor(UIColor.red.cgColor)
+            context.setFillColor(lambdaColor.cgColor)
+            context.setStrokeColor(lambdaColor.cgColor)
             context.fillPath()
             context.setLineJoin(.round)
             context.setLineCap(.round)
@@ -42,7 +44,40 @@ class LogoView: UIView {
             context.strokePath()
             context.addPath(path.cgPath)
             context.fillPath()
+            
+            let triangle = self.triangleFor(rect: CGRect(x: rect.midX - rect.midX / 2, y: rect.midY - rect.midY / 2, width: 100, height: 80))
+            context.setFillColor(UIColor.white.cgColor)
+            context.setStrokeColor(UIColor.white.cgColor)
+            context.setLineJoin(.round)
+            context.setLineCap(.round)
+            context.setLineWidth(borderRadiusInside)
+            context.addPath(triangle.cgPath)
+            context.strokePath()
+            context.addPath(triangle.cgPath)
+            context.fillPath()
         }
+    }
+    
+    private func triangleFor(rect: CGRect) -> UIBezierPath {
+        let path = UIBezierPath()
+        let topLeft = CGPoint(x: rect.midX - 10, y: rect.origin.y)
+        let topRight = CGPoint(x: rect.midX + 10, y: rect.origin.y)
+        let bottomRight2 = CGPoint(x: rect.maxX, y: rect.maxY)
+        let bottomRight1 = CGPoint(x: rect.maxX - 25, y: rect.maxY)
+        let center = CGPoint(x: rect.midX, y: rect.midY - 10)
+        let bottomLeft2 = CGPoint(x: rect.origin.y + 25, y: rect.maxY)
+        let bottomLeft1 = CGPoint(x: rect.origin.y, y: rect.maxY)
+        
+        path.move(to: topLeft)
+        path.addLine(to: topRight)
+        path.addLine(to: bottomRight2)
+        path.addLine(to: bottomRight1)
+        path.addLine(to: center)
+        path.addLine(to: bottomLeft2)
+        path.addLine(to: bottomLeft1)
+        path.close()
+        
+        return path
     }
 }
 
