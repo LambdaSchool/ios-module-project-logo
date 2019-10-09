58ec201d2030c969cd4e8d7ad98ff97785e718a8
Andrew Ruiz <nukedoom@gmail.com>
2019-10-09 14:22:16 -0700

    Initial Commit

---
+++ Project Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Logo
+//
+//  Created by Andrew Ruiz on 10/9/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
