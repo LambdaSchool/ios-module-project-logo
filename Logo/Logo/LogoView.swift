8b853b5ce15ee80fcb636fefc814f17454d45b14
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-11-06 15:32:01 -0800

    Added a curve to the logo background

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -23,30 +23,58 @@ class LogoView: UIView {
         if let context = UIGraphicsGetCurrentContext() {
             
             // First rect
-            context.addRect(CGRect(x: rect.origin.x,
+            
+            
+            let roundRect = CGRect(x: rect.origin.x,
                                    y: rect.origin.y,
                                    width: rect.size.width,
-                                   height: rect.size.height - heightOffSet))
+                                   height: rect.size.height - heightOffSet)
+            context.beginPath()
+            let roundedRectPath = CGPath(roundedRect: roundRect, cornerWidth: 10.0, cornerHeight: 10.0, transform: nil)
+            context.addPath(roundedRectPath)
+//            context.addRect(CGRect(x: rect.origin.x,
+//                                   y: rect.origin.y,
+//                                   width: rect.size.width,
+//                                   height: rect.size.height - heightOffSet))
             context.setFillColor(lambdaBgColor.cgColor)
             context.fillPath()
+            context.closePath()
+            
             
             let squareCenter = CGPoint(x: rect.size.width / 2.0,
                                        y: (rect.size.height - heightOffSet) / 2.0)
             
             // Triangle under square
-            context.beginPath()
-            context.move(to: CGPoint(x: rect.origin.x,
-                                     y: rect.size.height - heightOffSet))
-            context.addLine(to: CGPoint(x: squareCenter.x,
-                                        y: (rect.size.height - heightOffSet) + heightOffSet))
+            let curveOffSet: CGFloat = 5.0
             
-            context.addLine(to: CGPoint(x: rect.size.width,
-                                        y: rect.size.height - heightOffSet))
+            context.beginPath()
+            context.move(to: CGPoint(x: rect.origin.x + curveOffSet,
+                                     y: rect.size.height - heightOffSet - 1.5))
+            
+            context.addLine(to: CGPoint(x: squareCenter.x - curveOffSet * 2,
+                                        y: (rect.size.height - heightOffSet) + heightOffSet - curveOffSet))
+            context.addQuadCurve(to: CGPoint(x: squareCenter.x + curveOffSet * 2,
+                                               y: (rect.size.height - heightOffSet) + heightOffSet - curveOffSet),
+                                   control: CGPoint(x: squareCenter.x,
+                                                    y: (rect.size.height - heightOffSet) + heightOffSet))
+            
+            context.addLine(to: CGPoint(x: rect.size.width - curveOffSet,
+                                        y: rect.size.height - heightOffSet - 1.5))
+            
             context.closePath()
             context.setFillColor(lambdaBgColor.cgColor)
             context.fillPath()
             
+            //context.setStrokeColor(UIColor.black.cgColor)
+            context.beginPath()
+            context.move(to: CGPoint(x: squareCenter.x - 10,
+                                     y: (rect.size.height - heightOffSet) + heightOffSet - 10))
+            //context.setLineWidth(10)
+  
+            //context.strokePath()
+            context.closePath()
             
+            // Logo lines
             context.beginPath()
             context.move(to: CGPoint(x: squareCenter.x,
                                      y: squareCenter.y + logoWidthSize))
