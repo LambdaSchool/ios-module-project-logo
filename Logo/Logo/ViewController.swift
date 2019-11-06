20e3fc66a3790e651a6c5d0040621a9704997193
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2019-11-06 18:03:24 -0500

    Logo Done

---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by Dennis Rudolph on 11/6/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
