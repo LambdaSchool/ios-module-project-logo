5a990800aa49cd5d8faeaa67a526bc3dd0cf1a7b
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-11-06 14:14:15 -0800

    Completed lambda logo

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -14,7 +14,9 @@ class LogoView: UIView {
     private let lambdaBgColor = UIColor.red
     private let lambdaIconColor = UIColor.white
     
-    private let heightOffSet: CGFloat = 60.0
+    private let heightOffSet: CGFloat = 70.0
+    private let logoHeightSize: CGFloat = 30.0
+    private let logoWidthSize: CGFloat = 10.0
     
     override func draw(_ rect: CGRect) {
         // Drawing code
@@ -35,9 +37,8 @@ class LogoView: UIView {
             context.beginPath()
             context.move(to: CGPoint(x: rect.origin.x,
                                      y: rect.size.height - heightOffSet))
-            context.setLineWidth(2)
             context.addLine(to: CGPoint(x: squareCenter.x,
-                                        y: (rect.size.height - heightOffSet) + 60))
+                                        y: (rect.size.height - heightOffSet) + heightOffSet))
             
             context.addLine(to: CGPoint(x: rect.size.width,
                                         y: rect.size.height - heightOffSet))
@@ -45,6 +46,26 @@ class LogoView: UIView {
             context.setFillColor(lambdaBgColor.cgColor)
             context.fillPath()
             
+            
+            context.beginPath()
+            context.move(to: CGPoint(x: squareCenter.x,
+                                     y: squareCenter.y + logoWidthSize))
+            context.addLine(to: CGPoint(x: squareCenter.x - logoWidthSize * 3.5,
+                                        y: squareCenter.y + logoHeightSize * 2))
+            context.addLine(to: CGPoint(x: squareCenter.x - logoWidthSize * 7,
+                                        y: squareCenter.y + logoHeightSize * 2))
+            context.addLine(to: CGPoint(x: squareCenter.x - logoWidthSize * 1.25,
+                                        y: squareCenter.y - logoHeightSize))
+            context.addLine(to: CGPoint(x: squareCenter.x + logoWidthSize * 1.25,
+                                        y: squareCenter.y - logoHeightSize))
+            context.addLine(to: CGPoint(x: squareCenter.x + logoWidthSize * 7,
+                                        y: squareCenter.y + logoHeightSize * 2))
+            context.addLine(to: CGPoint(x: squareCenter.x + logoWidthSize * 3.5,
+                                        y: squareCenter.y + logoHeightSize * 2))
+            
+            context.closePath()
+            context.setFillColor(lambdaIconColor.cgColor)
+            context.fillPath()
         }
     }
 }
