6b0834957c04431fafe627ec7d55be28fdc26ae6
Dan M <djmthedev@gmail.com>
2019-11-06 16:21:49 -0800

    basic logo

---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by morse on 11/6/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
