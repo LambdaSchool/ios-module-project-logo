c7309e85f73aebaa8a2fc7c603b734b9ac5752d8
Jessie <threedaystoryllc@icloud.com>
2019-11-19 21:37:36 -0500

    Adds new project

---
+++ ios-module-project-logo-master/LogoDrawingPT3/LogoDrawingPT3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogoDrawingPT3
+//
+//  Created by Jessie Ann Griffin on 11/19/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
