15acb2068031a378c286fc6224bf3be88feb3b9c
Tyler Christian <tylerc71197@gmail.com>
2019-12-11 17:52:20 -0500

    Final Product

---
+++ ProjectLogo/ProjectLogo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectLogo
+//
+//  Created by Lambda_School_Loaner_218 on 12/11/19.
+//  Copyright © 2019 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
