91685a1df9369caad1e3bedbb0fda709b396fa83
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-22 23:28:28 +0000

    XOXO logo done

---
+++ Playstation Logo/Playstation Logo/LogoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogoViewController.swift
+//  Playstation Logo
+//
+//  Created by Tobi Kuyoro on 22/01/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+    
+    @IBOutlet weak var logoview: LogoView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
