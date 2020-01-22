b1b07c31f84456ffb033370e310ad641d7923178
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-01-22 15:54:15 -0800

    MVP

---
+++ CoreGraphicsLogo/CoreGraphicsLogo/Controller/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreGraphicsLogo
+//
+//  Created by Kenny on 1/22/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
