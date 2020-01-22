b3b0b03f01c97aa036603f025f49e95955c7bd14
Aaron Cleveland <toscleveland@gmail.com>
2020-01-22 16:55:00 -0600

    Cleaned up structure

---
+++ LogoAnimation/LogoAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogoAnimation
+//
+//  Created by Aaron Cleveland on 1/22/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
