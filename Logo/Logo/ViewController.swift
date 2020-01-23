acf0ace398e984378334bac1236f335f94f82c30
Michael McGrath <maybemichael@icloud.com>
2020-01-23 12:10:46 -0800

    Sort of Finished MVP

---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by Michael on 1/23/20.
+//  Copyright © 2020 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
