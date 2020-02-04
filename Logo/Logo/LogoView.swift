7eba1653748521f28134a4048f9f8b1d9745a9de
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-04 17:38:18 -0500

    Updated drawing to scale proportionally to view size

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -10,43 +10,44 @@ import UIKit
 
 
 
+
 @IBDesignable
 class LogoView: UIView {
     
     //MARK: - Properties
-    let color = UIColor.systemGray
-
-
+    let color = UIColor.white
+    
     // MARK: - View Lifecycle
     
-    
-     override func draw(_ rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         
         // Outer rectanglle
-        let rectangle = UIBezierPath(roundedRect: CGRect(x: rect.size.width / 2.0 - 50,
-                                                         y: rect.size.width / 2.0 - 25,
+        let rectangle = UIBezierPath(roundedRect: CGRect(x: self.frame.minX / 2.0 + 70,
+                                                         y: self.frame.minY / 2.0 - 15,
                                                          width: 100,
                                                          height: 100),
-                                                         cornerRadius: 30)
+                                     cornerRadius: 25)
+        
         color.setStroke()
-        rectangle.lineWidth = 10
+        rectangle.lineWidth = 15
         rectangle.lineJoinStyle = .round
         rectangle.stroke()
         
         // Inner circle
-        let circlePath = UIBezierPath(ovalIn: CGRect(x: rect.size.width / 2.0 - 25,
-                                                   y: rect.size.width / 2.0,
-                                                   width: 50,
-                                                   height: 50))
+        let circlePath = UIBezierPath(ovalIn: CGRect(x: self.frame.minX / 2.0 + 95,
+                                                     y: self.frame.minY / 2.0 + 10,
+                                                     width: 50,
+                                                     height: 50))
+        
         color.setStroke()
-        circlePath.lineWidth = 10
+        circlePath.lineWidth = 15
         circlePath.stroke()
         
         // Small circle
-        let smallCirclePath = UIBezierPath(ovalIn: CGRect(x: rect.size.width / 2.0 + 22,
-                                                    y: rect.size.width / 2.0 - 12,
-                                                    width: 13,
-                                                    height: 13))
+        let smallCirclePath = UIBezierPath(ovalIn: CGRect(x: self.frame.minX / 2.0 + 145,
+                                                          y: self.frame.minY / 2.0,
+                                                          width: 13,
+                                                          height: 13))
         color.setFill()
         smallCirclePath.fill()
         color.setStroke()
@@ -55,7 +56,9 @@ class LogoView: UIView {
         
         
         
+        
+        
     }
     
-
+    
 }
