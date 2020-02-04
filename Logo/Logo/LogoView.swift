c11d25f2520848b730ba32cde40d9f0525e72785
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-04 15:40:51 -0500

    Created LogoView

---
+++ Logo/Logo/LogoView.swift
@@ -0,0 +1,14 @@
+//
+//  LogoView.swift
+//  Logo
+//
+//  Created by Joshua Rutkowski on 2/4/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LogoView: UIView {
+    
+}
