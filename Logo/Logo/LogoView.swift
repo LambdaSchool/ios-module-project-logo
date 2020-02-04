9e93b60e8ce813ba491f846dcad6809e61c1c9c7
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-04 16:40:23 -0500

    Basic logo shape complete

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -16,40 +16,41 @@ class LogoView: UIView {
     //MARK: - Properties
     let color = UIColor.systemGray
 
+
     // MARK: - View Lifecycle
     
     
      override func draw(_ rect: CGRect) {
         
         // Outer rectanglle
-        let rectangle = UIBezierPath(roundedRect: CGRect(x: rect.origin.x,
-                                                         y: rect.origin.y,
+        let rectangle = UIBezierPath(roundedRect: CGRect(x: rect.size.width / 2.0 - 50,
+                                                         y: rect.size.width / 2.0 - 25,
                                                          width: 100,
                                                          height: 100),
-                                                         cornerRadius: 25)
+                                                         cornerRadius: 30)
         color.setStroke()
-        rectangle.lineWidth = 15
+        rectangle.lineWidth = 10
         rectangle.lineJoinStyle = .round
         rectangle.stroke()
         
         // Inner circle
-        let circlePath = UIBezierPath(ovalIn: CGRect(x: rect.origin.x,
-                                                   y: rect.origin.y,
+        let circlePath = UIBezierPath(ovalIn: CGRect(x: rect.size.width / 2.0 - 25,
+                                                   y: rect.size.width / 2.0,
                                                    width: 50,
                                                    height: 50))
         color.setStroke()
-        circlePath.lineWidth = 15
+        circlePath.lineWidth = 10
         circlePath.stroke()
         
         // Small circle
-        let smallCirclePath = UIBezierPath(ovalIn: CGRect(x: rect.origin.x,
-                                                    y: rect.origin.y,
+        let smallCirclePath = UIBezierPath(ovalIn: CGRect(x: rect.size.width / 2.0 + 22,
+                                                    y: rect.size.width / 2.0 - 12,
                                                     width: 13,
                                                     height: 13))
         color.setFill()
         smallCirclePath.fill()
         color.setStroke()
-        smallCirclePath.lineWidth = 15
+        smallCirclePath.lineWidth = 2
         smallCirclePath.stroke()
         
         
