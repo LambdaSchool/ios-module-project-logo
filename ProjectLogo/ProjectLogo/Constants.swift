5670b066e39586e81734c4053a96ad1526d086a8
Denis Cedeno <denis.cedeno@gmail.com>
2020-02-04 22:09:40 -0500

    completed Lambda logo

---
+++ ProjectLogo/ProjectLogo/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  ProjectLogo
+//
+//  Created by denis cedeno on 11/20/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    
+    static let iPurpleViolet     = UIColor(red: 138.0/255.0, green: 58.0/255.0, blue: 185.0/255.0, alpha: 1.0)
+    static let iRedOrange        = UIColor(red: 233.0/255.0, green: 89.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+    static let iOrange           = UIColor(red: 251.0/255.0, green: 173.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+    static let iBlue             = UIColor(red: 76.0/255.0, green: 104.0/255.0, blue: 215.0/255.0, alpha: 1.0)
+    static let iRedViolet       = UIColor(red: 188.0/255.0, green: 42.0/255.0, blue: 141.0/255.0, alpha: 1.0)
+    static let iMaroon          = UIColor(red: 205.0/255.0, green: 172.0/255.0, blue: 107.0/255.0, alpha: 1.0)
+    static let iYellow          = UIColor(red: 252.0/255.0, green: 204.0/255.0, blue: 99.0/255.0, alpha: 1.0)
+    static let lightGrey        = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+    static let black            = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let white            = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+}
