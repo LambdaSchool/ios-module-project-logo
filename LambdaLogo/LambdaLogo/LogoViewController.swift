42931f2ab41902078c792ede049207f0b31c6d9e
Eoin Lavery <me@eoinlavery.com>
2020-02-05 03:02:28 +0000

    Completed Lambda Logo drawing. Logo scales depending on size of UIView which is defined using Aspect Ratio constraints.

---
+++ LambdaLogo/LambdaLogo/LogoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogoViewController.swift
+//  LambdaLogo
+//
+//  Created by Eoin Lavery on 05/02/2020.
+//  Copyright © 2020 Eoin Lavery. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
