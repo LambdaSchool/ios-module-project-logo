499a311bc5d11ef5fadcc6abbf461c0a6dd065f5
Nichole Davidson <nicholedavid@gmail.com>
2020-03-18 14:38:00 -0600

    Added lifecycle code, logo background and border

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -6,12 +6,17 @@
 //  Copyright © 2020 Nichole Davidson. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 @IBDesignable
 class LogoView: UIView {
 
     // MARK: - Properties
+    private let logoBgColor = UIColor.red
+    private let letterColor = UIColor.white
+    private let borderColor = UIColor.red
+    private let borderWidth: CGFloat = 2.0
     
     
     // MARK: - View Lifecycle
@@ -23,7 +28,7 @@ class LogoView: UIView {
  
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        backgroundColor = UIColor.white
+        backgroundColor = UIColor.clear
     }
     
     
@@ -31,9 +36,21 @@ class LogoView: UIView {
     
     override func draw(_ rect: CGRect) {
        
+        if let context = UIGraphicsGetCurrentContext() {
+        
         // logo background
-        
-        
+            context.addRect(rect)
+            context.setFillColor(logoBgColor.cgColor)
+            context.fillPath()
+                     
+        // logo border
+            context.addRect(CGRect(x: rect.origin.x + borderWidth / 2.0,
+                                   y: rect.origin.y + borderWidth / 2.0,
+                                   width: rect.size.width - borderWidth,
+                                   height: rect.size.height - borderWidth))
+            context.setStrokeColor(borderColor.cgColor)
+            context.setLineWidth(borderWidth)
+            context.strokePath()
         
         
         // lambda letter
@@ -42,6 +59,6 @@ class LogoView: UIView {
         
         
     }
-    
+    }
 
 }
