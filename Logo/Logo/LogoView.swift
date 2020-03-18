8c659f3e51d11b1b02bcfb9751e09d4a7c779b3b
Shawn Gee <shawngee@gmail.com>
2020-03-18 16:25:59 -0400

    Used Color Slurp to input actual color values of logo

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -11,19 +11,20 @@ import UIKit
 @IBDesignable
 class LogoView: UIView {
 
+    private let cyan = CGColor(srgbRed: 11/255, green: 232/255, blue: 230/255, alpha: 1)
+    private let pink = CGColor(srgbRed: 255/255, green: 77/255, blue: 144/255, alpha: 1)
+    private let yellow = CGColor(srgbRed: 255/255, green: 175/255, blue: 22/255, alpha: 1)
     
     
     override func draw(_ rect: CGRect) {
         guard let context = UIGraphicsGetCurrentContext() else { return }
         
         context.translateBy(x: rect.midX, y: rect.midY)
+        context.setBlendMode(.multiply)
         
         let pillSize = CGSize(width: rect.width / 2, height: rect.width / 5.5)
         let pillOrigin = CGPoint(x: 0 - pillSize.width / 2, y: 0 )
         let pillFrame = CGRect(origin: pillOrigin, size: pillSize)
-        
-        
-        context.setBlendMode(.multiply)
         
         func drawPill(color: CGColor, rotation: CGFloat) {
             var tranform = CGAffineTransform(rotationAngle: rotation)
@@ -33,9 +34,9 @@ class LogoView: UIView {
             context.fillPath()
         }
         
-        drawPill(color: UIColor.cyan.cgColor, rotation: 0)
-        drawPill(color: UIColor.systemPink.cgColor, rotation: 2 * .pi / 3)
-        drawPill(color: UIColor.systemYellow.cgColor, rotation: -2 * .pi / 3)
+        drawPill(color: cyan, rotation: 0)
+        drawPill(color: pink, rotation: 2 * .pi / 3)
+        drawPill(color: yellow, rotation: -2 * .pi / 3)
     }
     
 
