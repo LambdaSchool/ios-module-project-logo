654f318d97472d21de5a1ce4506a301a347a63b6
Christopher DeVito <toolofjesus@protonmail.com>
2020-03-18 18:40:44 -0400

    right and left ovals drawn

--- DrawLogo/DrawLogo/Controllers/LogoController.swift
+++ DrawLogo/DrawLogo/Controllers/LogoController.swift
@@ -33,18 +33,33 @@ class LogoView: UIView {
             // white loops
 
             // left loop
-            //create rotated rect
-            let leftRect = CGRect(x: rect.origin.x + 200,
-                                  y: rect.origin.y + 10,
-                                  width: 50,
-                                  height: 150).applying(CGAffineTransform(rotationAngle: .pi/4))
-            
-                
-            
-            // create oval in left rect
-            context.addEllipse(in: leftRect)
+            context.saveGState()
+            context.rotate(by: -20 * CGFloat.pi/180)
+            let leftOvalPath = UIBezierPath(ovalIn: CGRect(x: 35, y: 65, width: 70, height: 100))
             context.setFillColor(UIColor.white.cgColor)
-            context.fillPath()
+            leftOvalPath.fill()
+            context.restoreGState()
+            
+            // right loop
+            context.saveGState()
+            context.rotate(by: 20 * CGFloat.pi/180)
+            let rightOvalPath = UIBezierPath(ovalIn: CGRect(x: 205, y: -50, width: 70, height: 100))
+            context.setFillColor(UIColor.white.cgColor)
+            rightOvalPath.fill()
+            context.restoreGState()
+            
+            // left exit from loop
+            context.saveGState()
+            context.translateBy(x: 94, y: 24)
+            context.rotate(by: 40 * CGFloat.pi/180)
+
+            let rectanglePath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 39, height: 62))
+            UIColor.gray.setFill()
+            rectanglePath.fill()
+
+            context.restoreGState()
+            
+            // right exit from loop
 
         }
     }
