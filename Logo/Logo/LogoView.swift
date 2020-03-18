af052e7b3aa37dea46b1aab0e092628731547934
Mark Gerrior <github@gerrior.com>
2020-03-18 14:35:10 -0700

    OK, dot on the screen. Boilerplate in place.

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -26,7 +26,19 @@ class LogoView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
         /// Note: elements are drawn on the the screen from back to front
         /// in the order they appear below.
+        
+        // Get a dot on the screen for proof of concept
+        let smallDotRadius: CGFloat = 12.0
+        let smallCircle = CGRect(x: rect.midX - smallDotRadius,
+                                 y: rect.midY - smallDotRadius,
+                                 width: smallDotRadius * 2,
+                                 height: smallDotRadius * 2)
+        context.addEllipse(in: smallCircle)
+        context.setFillColor(UIColor.blue.cgColor)
+        context.fillPath()
     }
 }
