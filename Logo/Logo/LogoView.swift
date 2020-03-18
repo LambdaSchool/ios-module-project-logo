9631dd083ed50dfd9532e2cbaf7f8448a9f9a6fa
Nichole Davidson <nicholedavid@gmail.com>
2020-03-18 14:14:32 -0600

    Initial setup

---
+++ Logo/Logo/LogoView.swift
@@ -0,0 +1,47 @@
+//
+//  LogoView.swift
+//  Logo
+//
+//  Created by Nichole Davidson on 3/18/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LogoView: UIView {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - View Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.white
+    }
+ 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = UIColor.white
+    }
+    
+    
+    // MARK: - Draw
+    
+    override func draw(_ rect: CGRect) {
+       
+        // logo background
+        
+        
+        
+        
+        // lambda letter
+        
+        
+        
+        
+    }
+    
+
+}
---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by Nichole Davidson on 3/18/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
