d199704213535f892cb6070060789d2551e60db0
Mark Gerrior <github@gerrior.com>
2020-03-18 15:51:04 -0700

    Two parts of Lambda logo

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Foundation
 
 @IBDesignable // Interface Builder will render the view directly in the canvas
 class LogoView: UIView {
 
+    let debug = 1.0
+    
     // MARK: - View Lifecycle
     
     override init(frame: CGRect) {
@@ -20,7 +23,7 @@ class LogoView: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        backgroundColor = UIColor.clear
+//        backgroundColor = UIColor.clear
     }
     
     // Only override draw() if you perform custom drawing.
@@ -32,13 +35,47 @@ class LogoView: UIView {
         /// in the order they appear below.
         
         // Get a dot on the screen for proof of concept
-        let smallDotRadius: CGFloat = 12.0
-        let smallCircle = CGRect(x: rect.midX - smallDotRadius,
-                                 y: rect.midY - smallDotRadius,
-                                 width: smallDotRadius * 2,
-                                 height: smallDotRadius * 2)
-        context.addEllipse(in: smallCircle)
-        context.setFillColor(UIColor.blue.cgColor)
+//        let smallDotRadius: CGFloat = 12.0
+//        let smallCircle = CGRect(x: rect.midX - smallDotRadius,
+//                                 y: rect.midY - smallDotRadius,
+//                                 width: smallDotRadius * 2,
+//                                 height: smallDotRadius * 2)
+//        context.addEllipse(in: smallCircle)
+//        context.setFillColor(UIColor.blue.cgColor)
+//        context.fillPath()
+        
+        // Top rectangle
+        let rect = CGRect(x: 10, y: 10, width: 100, height: 78)
+        context.addRect(rect)
+        context.setFillColor(UIColor.red.cgColor)
         context.fillPath()
+
+        // Bottom Triangle
+        let offset = 10.0
+        
+        let startPoint  = CGPoint(x: offset,        y: offset + 78 + debug)
+        let nextPoint   = CGPoint(x: offset +  50,  y: offset + 78 + 37 + debug)
+        let endPoint    = CGPoint(x: offset + 100,  y: offset + 78 + debug)
+        context.beginPath()
+        context.move(to: startPoint)
+        context.setFillColor(UIColor.red.cgColor)
+        context.addLine(to: nextPoint)
+        context.addLine(to: endPoint)
+        context.closePath()
+        context.fillPath()
+
+        
+//        context.setStrokeColor(hours.color.cgColor)
+//        context.beginPath()
+//        context.move(to: clockCenter)
+//        context.setLineWidth(hours.width)
+//        context.addLine(to: hourHandEndPoint)
+//        context.strokePath()
+//
+//        path.move(to: startPoint)
+//        path.addLine(to: nextPoint)
+//        path.addLine(to: endPoint)
+//        path.closeSubpath()
+
     }
 }
