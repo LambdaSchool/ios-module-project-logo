f59121ffb72c701bc62eb93255ea9b8cda19fb3a
Hector L <hectoremil21@hotmail.com>
2020-03-18 17:54:42 -0400

    Finish square

--- LogoView/LogoView/LogoView.swift
+++ LogoView/LogoView/LogoView.swift
@@ -20,6 +20,8 @@ class LogoView: UIView {
             
             // MARK: - Properties
             let cornerRadius = CGFloat(10)
+            let squareHeight = CGFloat(rect.origin.y + rect.width * 0.6)
+            let triangleHeight = CGFloat(squareHeight + squareHeight * 0.5)
             
             // MARK: - Top left rounded Corner
             let topLeftControlPoint = CGPoint(x: rect.origin.x + rect.width * 0.15 , y: rect.origin.y + rect.height * 0.1)
@@ -30,6 +32,16 @@ class LogoView: UIView {
             let topRightControlPoint = CGPoint(x: rect.origin.x + rect.width * 0.85, y: rect.origin.y + rect.height * 0.1)
             context.addLine(to: CGPoint(x: topRightControlPoint.x - cornerRadius, y: topRightControlPoint.y))
             context.addQuadCurve(to: CGPoint(x: topRightControlPoint.x, y: topRightControlPoint.y + cornerRadius), control: topRightControlPoint)
+            
+            // MARK: - Bottom right rounded Corner
+            let bottomRightControlPoint = CGPoint(x: topRightControlPoint.x, y: squareHeight)
+            context.addLine(to: CGPoint(x: bottomRightControlPoint.x, y: bottomRightControlPoint.y - cornerRadius))
+            context.addQuadCurve(to: CGPoint(x: bottomRightControlPoint.x - cornerRadius, y: bottomRightControlPoint.y), control: bottomRightControlPoint)
+            
+            // MARK: - Bottom left rounded Corner
+            let bottomLeftControlPoint = CGPoint(x: topLeftControlPoint.x, y: squareHeight)
+            context.addLine(to: CGPoint(x: bottomLeftControlPoint.x + cornerRadius, y: bottomLeftControlPoint.y))
+            context.addQuadCurve(to: CGPoint(x: bottomLeftControlPoint.x, y: bottomLeftControlPoint.y - cornerRadius), control: bottomLeftControlPoint)
             
             context.setFillColor(UIColor.blue.cgColor)
             context.fillPath()
