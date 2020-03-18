c166acf3156ac1a301232915b81fb54965d28224
Mark Gerrior <github@gerrior.com>
2020-03-18 14:30:08 -0700

    Getting more baseline stuff in there.

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
+@IBDesignable // Interface Builder will render the view directly in the canvas
 class LogoView: UIView {
 
-    /*
+    // MARK: - View Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = UIColor.clear
+    }
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        /// Note: elements are drawn on the the screen from back to front
+        /// in the order they appear below.
     }
-    */
-
 }
