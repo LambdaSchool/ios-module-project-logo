167064eec1f0880cdfaa0a933a20d5a578238438
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-18 16:20:38 -0400

    added some lines to a canvas

--- Logo/Logo/LambdaLogoView.swift
+++ Logo/Logo/LambdaLogoView.swift
@@ -24,10 +24,13 @@ class LambdaLogoView: UIView {
      }
      
      */
+    
+    let red = UIColor(red:0.73, green:0.07, blue:0.20, alpha:1.00)
      
     override func draw(_ rect: CGRect) {
         if let context = UIGraphicsGetCurrentContext() {
             
+            // Rectangle
             let rectangle = CGRect(x: rect.minX, y: rect.minY, width: rect.width, height: (rect.height / 3) * 2)
             let roundedRectanglePath = CGPath(roundedRect: rectangle,
                                         cornerWidth: 6.0,
@@ -37,20 +40,33 @@ class LambdaLogoView: UIView {
             context.setFillColor(UIColor.white.cgColor)
             context.fillPath()
             
+            // Triangle
             let leftCorner = CGPoint(x: rectangle.minX + 5, y: rectangle.maxY)
             let rightCorner = CGPoint(x: rectangle.maxX - 5, y: rectangle.maxY)
             let centerBottom = CGPoint(x: rect.midX, y: rect.maxY)
             context.setStrokeColor(UIColor.white.cgColor)
             context.beginPath()
             context.move(to: leftCorner)
-            context.setLineWidth(3)
             context.addLine(to: centerBottom)
             context.addLine(to: rightCorner)
             context.addLine(to: leftCorner)
             context.closePath()
-            //context.strokePath()
             context.setFillColor(UIColor.white.cgColor)
             context.fillPath()
+            context.strokePath()
+
+            // Lambda
+            let middle = CGPoint(x: rectangle.midX, y: rectangle.midY)
+            context.setStrokeColor(red.cgColor)
+            context.beginPath()
+            context.move(to: middle)
+            context.setLineWidth(5)
+            context.addLine(to: CGPoint(x: rectangle.midX - 50, y: rectangle.midY + 75))
+            context.addLine(to: CGPoint(x: rectangle.midX - 75, y: rectangle.midY + 75))
+            
+            context.strokePath()
+
+            
             
             
         }
