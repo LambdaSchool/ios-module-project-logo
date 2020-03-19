aba170d76079681967cf15c4606b5bdeb27ba665
Nichole Davidson <nicholedavid@gmail.com>
2020-03-18 20:42:57 -0600

    Cleaned things up and finished

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -11,14 +11,10 @@ import UIKit
 
 @IBDesignable
 class LogoView: UIView {
-
+    
     // MARK: - Properties
-    private let logoBgColor = UIColor.red
+    private let logoBgColor = UIColor.init(red: 0.75, green: 0.10, blue: 0.20, alpha: 1.0)
     private let letterColor = UIColor.white
-    private let borderColor = UIColor.red
-    private let borderWidth: CGFloat = 2.0
-    
-    
     
     // MARK: - View Lifecycle
     
@@ -26,16 +22,15 @@ class LogoView: UIView {
         super.init(frame: frame)
         backgroundColor = UIColor.white
     }
- 
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    
     // MARK: - Draw
     
     override func draw(_ rect: CGRect) {
-       
+        
         let roundedRectPath = CGPath(roundedRect: rect, cornerWidth: 8.0, cornerHeight: 8.0, transform: nil)
         let path = CGMutablePath()
         let leftCorner = CGPoint(x: rect.minX, y: rect.maxY * 0.65)
@@ -47,34 +42,31 @@ class LogoView: UIView {
         let letterLeftCorner = CGPoint(x: rect.maxX * 0.20, y: rect.maxY * 0.58)
         let letterRightCorner = CGPoint(x: rect.maxX * 0.80, y: rect.maxY * 0.58)
         let letterTopPoint = CGPoint(x: rect.midX, y: rect.maxY / 6.5)
-       
-        // inside diagnol up
         
+        let innerLeft = CGPoint(x: rect.maxX * 0.35, y: rect.maxY * 0.58)
+        let innerRight = CGPoint(x: rect.maxX * 0.65, y: rect.maxY * 0.58)
+        let innerTop = CGPoint(x: rect.midX, y: rect.maxY / 2.8)
         
-        // inside diagnol down
-        
-        
-        
+        let topLetterRect = CGRect(x: rect.midX * 0.50, y: rect.maxY / 6.5, width: rect.maxX * 0.50, height: rect.maxY * 0.07)
         
         if let context = UIGraphicsGetCurrentContext() {
-                               
-        // logo background
+            
+            // logo background
             context.beginPath()
             context.addPath(roundedRectPath)
             context.setFillColor(letterColor.cgColor)
             context.fillPath()
             
+            // logo bottom point
             
-        // logo bottom point
-           
             context.beginPath()
             context.move(to: leftCorner)
             context.addLine(to: pointCorner)
             context.addLine(to: rightCorner)
             context.setFillColor(logoBgColor.cgColor)
             context.fillPath()
-                        
-        // logo top portion
+            
+            // logo top portion
             context.beginPath()
             context.move(to: leftCorner)
             context.addLine(to: topLeftCorner)
@@ -83,7 +75,7 @@ class LogoView: UIView {
             context.setFillColor(logoBgColor.cgColor)
             context.fillPath()
             
-        // lambda letter - outer
+            // lambda letter - outer
             
             context.beginPath()
             context.move(to: letterLeftCorner)
@@ -93,14 +85,23 @@ class LogoView: UIView {
             context.setFillColor(letterColor.cgColor)
             context.fillPath()
             
-        // lambda letter inner
+            // lambda letter inner
             
+            context.beginPath()
+            context.move(to: innerLeft)
+            context.addLine(to: innerRight)
+            context.addLine(to: innerTop)
+            context.addLine(to: innerLeft)
+            context.setFillColor(logoBgColor.cgColor)
+            context.fillPath()
             
             // lambda letter top
-        
-        
-        
+            
+            context.beginPath()
+            context.addRect(topLetterRect)
+            context.setFillColor(logoBgColor.cgColor)
+            context.fillPath()
+        }
     }
-    }
-
+    
 }
