2780a4d07a0894e4dd27c07063595dac5d1dbb62
Chris Dobek <dobek.christoher@gmail.com>
2020-04-15 16:56:15 -0400

    Logo MVP

---
+++ Logo/Logo/LogoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LogoViewController.swift
+//  Logo
+//
+//  Created by Chris Dobek on 4/15/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        }
+
+}
