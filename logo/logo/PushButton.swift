d7b87c53e8ba88a6550ccad8b24bec2b5bb4528d
Claudia Maciel <clc_80@yahoo.com>
2020-04-14 20:17:20 -0700

    Added Lambda LogoView

--- logo/logo/PushButton.swift
+++ logo/logo/PushButton.swift
@@ -11,6 +11,9 @@ import UIKit
 @IBDesignable
 class PushButton: UIButton {
     
+    @IBInspectable var fillColor: UIColor = UIColor.green
+    @IBInspectable var isAddButton: Bool = true
+    
     private struct Constants {
         static let plusLineWidth: CGFloat = 3.0
         static let plusButtonScale: CGFloat = 0.6
@@ -27,7 +30,7 @@ class PushButton: UIButton {
 
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath(ovalIn: rect)
-        UIColor.blue.setFill()
+        fillColor.setFill()
         path.fill()
         
         // Set up the width and height variables for the horizontal stroke
@@ -50,15 +53,16 @@ class PushButton: UIButton {
             x: halfWidth + halfPlusWidth + Constants.halfPointShift,
             y: halfHeight + Constants.halfPointShift))
         
-        // Vertical stroke
-        plusPath.move(to: CGPoint(
-            x: halfWidth + Constants.halfPointShift,
-            y: halfHeight - halfPlusWidth + Constants.halfPointShift))
-        
-        plusPath.addLine(to: CGPoint(
-            x: halfWidth + Constants.halfPointShift,
-            y: halfHeight + halfPlusWidth + Constants.halfPointShift))
-        
+        if isAddButton {
+            // Vertical stroke
+            plusPath.move(to: CGPoint(
+                x: halfWidth + Constants.halfPointShift,
+                y: halfHeight - halfPlusWidth + Constants.halfPointShift))
+            
+            plusPath.addLine(to: CGPoint(
+                x: halfWidth + Constants.halfPointShift,
+                y: halfHeight + halfPlusWidth + Constants.halfPointShift))
+        }
         // Set the stroke color
         UIColor.white.setStroke()
         
