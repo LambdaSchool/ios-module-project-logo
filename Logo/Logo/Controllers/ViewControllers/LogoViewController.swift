9f8dae1279a39d57d91318e48f410c91d765671e
James McDougall <jameswiliammcdougall@gmail.com>
2020-04-14 21:12:37 -0700

    MVP

---
+++ Logo/Logo/Controllers/ViewControllers/LogoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LogoViewController.swift
+//  Logo
+//
+//  Created by Lambda_School_loaner_226 on 4/14/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+    
+    @IBOutlet weak var logoView: LogoView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+
+}
