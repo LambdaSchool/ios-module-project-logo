b9dd4f57cf82b051c97eb660907a4e4213bc2d84
Matt Martindale <martindalematt18@yahoo.com>
2020-04-14 18:31:49 -0600

    Logo MVP finished

--- Logo/Logo/Views/LogoView.swift
+++ Logo/Logo/Views/LogoView.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 let lambdaRed: UIColor = #colorLiteral(red: 0.7294117647, green: 0.07843137255, blue: 0.2039215686, alpha: 1)
+let lambdaString: NSString = "Λ"
 
 @IBDesignable
 class LogoView: UIView {
@@ -28,6 +29,7 @@ class LogoView: UIView {
         
         if let context = UIGraphicsGetCurrentContext() {
             
+            //Lambda Background
             let lambdaRect = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.size.width, height: rect.size.height * 0.6)
             let clipPath = UIBezierPath(roundedRect: lambdaRect, cornerRadius: 10).cgPath
             context.addPath(clipPath)
@@ -37,7 +39,6 @@ class LogoView: UIView {
             
             context.beginPath()
             context.move(to: CGPoint(x: rect.origin.x, y: rect.size.height * 0.6 - rect.size.height * 0.02))
-//            context.addLine(to: CGPoint(x: rect.size.width / 2, y: rect.size.height))
             context.addCurve(to: CGPoint(x: rect.size.width / 2 + 10,
                                          y: rect.size.height - 10),
                              control1: CGPoint(x: rect.size.width / 2 + 8,
@@ -47,10 +48,19 @@ class LogoView: UIView {
             context.addLine(to: CGPoint(x: rect.size.width, y: rect.size.height * 0.6 - rect.size.height * 0.02))
             context.closePath()
             
-//            context.
-            
             context.setFillColor(lambdaRed.cgColor)
             context.fillPath()
+            
+            //Lambda Character
+            let lambdaStringRect = CGRect(x: rect.size.width * 0.25,
+                                          y: rect.size.height * 0.07,
+                                          width: rect.size.width / 2,
+                                          height: rect.size.height / 2)
+//            let lambdaStringRect = CGPoint(x: rect.size.width / 2 - lambdaString., y: lambdaRect.size.height / 2)
+//            lambdaString.draw(at: lambdaCenter, withAttributes: [NSAttributedString.Key.foregroundColor: UIColor.white,
+//                                                                 NSAttributedString.Key.font: UIFont.systemFont(ofSize: 200, weight: .black)])
+            lambdaString.draw(in: lambdaStringRect, withAttributes: [NSAttributedString.Key.foregroundColor: UIColor.white,
+                                                                     NSAttributedString.Key.font: UIFont.systemFont(ofSize: 200, weight: .black)])
         }
     }
     
