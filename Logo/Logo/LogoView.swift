9d8dc6885b621344522e13b5156667e6f8a5e11b
Chris Dobek <dobek.christoher@gmail.com>
2020-04-15 15:37:50 -0400

    Initial Logo Commit

---
+++ Logo/Logo/LogoView.swift
@@ -0,0 +1,21 @@
+//
+//  LogoView.swift
+//  Logo
+//
+//  Created by Chris Dobek on 4/15/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class LogoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
