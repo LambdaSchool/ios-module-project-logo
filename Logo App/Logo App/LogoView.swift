6f9c09ff4be3484c360b940159e0824aec963d40
Jarren Campos <jarrenmcampos@gmail.com>
2020-04-14 18:54:15 -0700

    Finished Lambda Logo Project

--- Logo App/Logo App/LogoView.swift
+++ Logo App/Logo App/LogoView.swift
@@ -31,15 +31,22 @@ class LogoView: UIView {
             
             //MARK: Rect Properties
             //Add in positions to use dynamically
+            let width = rect.width
+            let height = rect.height
+            let origin: CGFloat = 0.0
+            let twoThirds: CGFloat = height / 1.5
+            let xCenter = width / 2
+            let fourthWidth = width / 4
+            let fourthHeight = height / 4
             
             //Red background logo
             context.beginPath()
             context.move(to: rect.origin)
-            context.addLine(to: CGPoint(x: rect.width,y: 0.0))
-            context.addLine(to: CGPoint(x: rect.width,y: rect.height / 1.5))
-            context.addLine(to: CGPoint(x: rect.width / 2,y: rect.height))
-            context.addLine(to: CGPoint(x: 0.0, y: rect.height / 1.5))
-            context.addLine(to: CGPoint(x: 0.0, y: 0.0))
+            context.addLine(to: CGPoint(x: width, y: origin))
+            context.addLine(to: CGPoint(x: width, y: twoThirds))
+            context.addLine(to: CGPoint(x: xCenter, y: height))
+            context.addLine(to: CGPoint(x: origin, y: twoThirds))
+            context.addLine(to: CGPoint(x: origin, y: origin))
             context.setStrokeColor(lambdaBgdColor)
             context.setFillColor(lambdaBgdColor)
             context.fillPath()
@@ -48,9 +55,9 @@ class LogoView: UIView {
             
             //White logo part
             context.beginPath()
-            context.move(to: CGPoint(x: rect.width / 4, y: rect.height / 1.50))
-            context.addLine(to: CGPoint(x: rect.width / 2, y: rect.height / 4))
-            context.addLine(to: CGPoint(x: 180, y: 170))
+            context.move(to: CGPoint(x: fourthWidth, y: twoThirds))
+            context.addLine(to: CGPoint(x: xCenter, y: fourthHeight))
+            context.addLine(to: CGPoint(x: fourthWidth * 3, y: twoThirds))
             context.setLineWidth(20)
             context.setStrokeColor(lambdaInColor)
             context.strokePath()
