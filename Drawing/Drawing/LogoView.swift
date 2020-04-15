d68742b6588f2bbb147f338d6b649f9325a0eaa5
kensam1 <samarakodykennedy@gmail.com>
2020-04-14 23:41:36 -0400

    Drawing Project

---
+++ Drawing/Drawing/LogoView.swift
@@ -0,0 +1,23 @@
+//
+//  LogoView.swift
+//  Drawing
+//
+//  Created by Lambda_School_Loaner_241 on 4/14/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import UIKit
+
+class LogoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
+
+// start here tmrw
