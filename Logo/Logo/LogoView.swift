3394b30b10ffcb4c6a2b7fcc3d0392bfaffb2c21
Libby Thomas <thomaslibby05@gmail.com>
2020-04-15 00:01:50 -0500

    Labels and color changes added
    Added labels and buttons to change the color of the labels

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -6,16 +6,128 @@
 //  Copyright © 2020 Libby Thomas. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
+@IBDesignable
 class LogoView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - Properites
+    private var logoView = UIView()
+    private var logoViewController: LogoViewController?
+    
+    private let logoColor = UIColor.black.cgColor
+    private let logoLineWidth: CGFloat = 5.0
+    
+    // Leaf
+    private let leafTopPoint = CGPoint(x: 250, y: 50)
+    private let leafBottomPoint = CGPoint(x: 200, y: 100)
+    
+    private let leafLeftControlPoint = CGPoint(x: 200, y: 60)
+    private let leafRightControlPoint = CGPoint(x: 250, y: 100)
+    
+    // Apple top line
+    private let appleTopLeftPoint = CGPoint(x: 50, y: 135)
+    private let appleTopMiddlePoint = CGPoint(x: 200, y: 140)
+    private let appleTopRightPoint = CGPoint(x: 350, y: 140)
+    
+    private let appleTopRightControlPoint = CGPoint(x: 280, y: 80)
+    private let appleTopLeftControlPoint = CGPoint(x: 110, y: 80)
+    
+    // Apple left line
+    private let appleLeftMiddlePoint = CGPoint(x: 40, y: 355)
+    private let appleFirstLeftControlPoint = CGPoint(x: 0, y: 180)
+    private let appleSecondLeftControlPoint = CGPoint(x: 0, y: 300)
+    
+    // Apple bottom line
+    private let appleBottomLeftPoint = CGPoint(x: 75, y: 385)
+    private let appleBottomMiddlePoint = CGPoint(x: 200, y: 380)
+    private let appleBottomRightPoint = CGPoint(x: 300, y: 390)
+    
+    private let appleBottomLeftControlPoint = CGPoint(x: 60, y: 380)
+    private let appleFirstBottomLeftControlPoint = CGPoint(x: 140, y: 410)
+    private let appleSecondBottomLeftControlPoint = CGPoint(x: 150, y: 380)
+    
+    private let appleFirstBottomRightCP = CGPoint(x: 235, y: 380)
+    private let appleSecondBottomRightCP = CGPoint(x: 255, y: 410)
+    
+    // Apple right line
+    private let appleMiddleRightPoint = CGPoint(x: 350, y: 320)
+    private let appleFirstRightCP = CGPoint(x: 335, y: 365)
+    private let appleSecondRightCP = CGPoint(x: 345, y: 335)
+    
+    // Apple bite line
+    private let appleBiteMiddlePoint = CGPoint(x: 295, y: 230)
+    private let appleBiteTopCP = CGPoint(x: 295, y: 180)
+    private let appleBiteBottomCP = CGPoint(x: 295, y: 310)
+    
+    
+    // MARK: - View Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.white
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        backgroundColor = UIColor.white
+    }
+    
+    
 
+    override func draw(_ rect: CGRect) {
+        if let context = UIGraphicsGetCurrentContext() {
+            
+            // Leaf top line
+            context.beginPath()
+            context.move(to: leafTopPoint)
+            context.addQuadCurve(to: leafBottomPoint, control: leafLeftControlPoint)
+            
+            // Leaf bottom line
+            context.move(to: leafTopPoint)
+            context.addQuadCurve(to: leafBottomPoint, control: leafRightControlPoint)
+            
+            // Apple top lines
+            context.move(to: appleTopRightPoint)
+            context.addQuadCurve(to: appleTopMiddlePoint, control: appleTopRightControlPoint)
+
+            context.move(to: appleTopMiddlePoint)
+            context.addQuadCurve(to: appleTopLeftPoint, control: appleTopLeftControlPoint)
+            
+            // Apple left line
+
+            context.move(to: appleTopLeftPoint)
+            context.addCurve(to: appleLeftMiddlePoint, control1: appleFirstLeftControlPoint, control2: appleSecondLeftControlPoint)
+            
+            // Apple bottom lines
+
+            context.move(to: appleLeftMiddlePoint)
+            context.addQuadCurve(to: appleBottomLeftPoint, control: appleBottomLeftControlPoint)
+            
+            context.move(to: appleBottomLeftPoint)
+            context.addCurve(to: appleBottomMiddlePoint, control1: appleFirstBottomLeftControlPoint, control2: appleSecondBottomLeftControlPoint)
+
+            context.move(to: appleBottomMiddlePoint)
+            context.addCurve(to: appleBottomRightPoint, control1: appleFirstBottomRightCP, control2: appleSecondBottomRightCP)
+            
+            // Apple right line
+
+            context.move(to: appleBottomRightPoint)
+            context.addCurve(to: appleMiddleRightPoint, control1: appleFirstRightCP, control2: appleSecondRightCP)
+            
+            context.move(to: appleTopRightPoint)
+            context.addQuadCurve(to: appleBiteMiddlePoint, control: appleBiteTopCP)
+            
+            context.move(to: appleMiddleRightPoint)
+            context.addQuadCurve(to: appleBiteMiddlePoint, control: appleBiteBottomCP)
+            
+            context.setStrokeColor(logoColor)
+            context.setLineWidth(logoLineWidth)
+            context.strokePath()
+
+        }
+    }
 }
--- Logo/Logo/LogoViewController.swift
+++ Logo/Logo/LogoViewController.swift
@@ -9,9 +9,169 @@
 import UIKit
 
 class LogoViewController: UIViewController {
-
+    
+    @IBOutlet weak var logoView: LogoView!
+    private var aLabel = UILabel()
+    private var firstPLabel = UILabel()
+    private var secondPLabel = UILabel()
+    private var lLabel = UILabel()
+    private var eLabel = UILabel()
+    
+    var redButtonToggled: Bool = false
+    var orangeButtonToggled: Bool = false
+    var yellowButtonToggled: Bool = false
+    var greenButtonToggled: Bool = false
+    var blueButtonToggled: Bool = false
+    var indigoButtonToggled: Bool = false
+    var violetButtonToggled: Bool = false
+    var blackButtonToggled: Bool = false
+    
+    
+    // MARK: - Live Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLabels()
     }
+    
+    // MARK: - IBActions
+    
+    private func configureLabels() {
+        aLabel.translatesAutoresizingMaskIntoConstraints = false
+        aLabel.text = "A"
+        aLabel.font = UIFont(name: "American Typewriter", size: 60)
+        
+        firstPLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstPLabel.text = "p"
+        firstPLabel.font = UIFont(name: "American Typewriter", size: 60)
+        
+        secondPLabel.translatesAutoresizingMaskIntoConstraints = false
+        secondPLabel.text = "p"
+        secondPLabel.font = UIFont(name: "American Typewriter", size: 60)
+        
+        lLabel.translatesAutoresizingMaskIntoConstraints = false
+        lLabel.text = "l"
+        lLabel.font = UIFont(name: "American Typewriter", size: 60)
+        
+        eLabel.translatesAutoresizingMaskIntoConstraints = false
+        eLabel.text = "e"
+        eLabel.font = UIFont(name: "American Typewriter", size: 60)
+        
+        view.addSubview(aLabel)
+        view.addSubview(firstPLabel)
+        view.addSubview(secondPLabel)
+        view.addSubview(lLabel)
+        view.addSubview(eLabel)
+        
+        NSLayoutConstraint.activate([
+        
+            aLabel.leadingAnchor.constraint(equalTo: logoView.leadingAnchor, constant: 70),
+            aLabel.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: 20),
+            
+            firstPLabel.leadingAnchor.constraint(equalTo: aLabel.trailingAnchor, constant: 20),
+            firstPLabel.topAnchor.constraint(equalTo: aLabel.topAnchor),
+            
+            secondPLabel.leadingAnchor.constraint(equalTo: firstPLabel.trailingAnchor, constant: 20),
+            secondPLabel.topAnchor.constraint(equalTo: aLabel.topAnchor),
+            
+            lLabel.leadingAnchor.constraint(equalTo: secondPLabel.trailingAnchor, constant: 20),
+            lLabel.topAnchor.constraint(equalTo: aLabel.topAnchor),
+            
+            eLabel.leadingAnchor.constraint(equalTo: lLabel.trailingAnchor, constant: 20),
+            eLabel.topAnchor.constraint(equalTo: aLabel.topAnchor),
+            eLabel.trailingAnchor.constraint(equalTo: logoView.trailingAnchor, constant: 20)
+        
+        ])
+        
+    }
+    
+    @IBAction func rainbowButtonTapped(_ sender: Any) {
+        aLabel.textColor = .red
+        firstPLabel.textColor = .orange
+        secondPLabel.textColor = .yellow
+        lLabel.textColor = .blue
+        eLabel.textColor = .green
+    }
+    
+    @IBAction func redButtonTapped(_ sender: Any) {
+        aLabel.textColor = .red
+        firstPLabel.textColor = .red
+        secondPLabel.textColor = .red
+        lLabel.textColor = .red
+        eLabel.textColor = .red
+        
+        redButtonToggled = true
+    }
+    
+    @IBAction func orangeButtonTapped(_ sender: Any) {
+        aLabel.textColor = .orange
+        firstPLabel.textColor = .orange
+        secondPLabel.textColor = .orange
+        lLabel.textColor = .orange
+        eLabel.textColor = .orange
+        
+        orangeButtonToggled = true
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        aLabel.textColor = .yellow
+        firstPLabel.textColor = .yellow
+        secondPLabel.textColor = .yellow
+        lLabel.textColor = .yellow
+        eLabel.textColor = .yellow
+        
+        yellowButtonToggled = true
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        aLabel.textColor = .green
+        firstPLabel.textColor = .green
+        secondPLabel.textColor = .green
+        lLabel.textColor = .green
+        eLabel.textColor = .green
+        
+        greenButtonToggled = true
+    }
+    
+    @IBAction func blueButtonTapped(_ sender: Any) {
+        aLabel.textColor = .blue
+        firstPLabel.textColor = .blue
+        secondPLabel.textColor = .blue
+        lLabel.textColor = .blue
+        eLabel.textColor = .blue
+        
+        blueButtonToggled = true
+    }
+    
+    @IBAction func indigoButtonTapped(_ sender: Any) {
+        aLabel.textColor = .systemIndigo
+        firstPLabel.textColor = .systemIndigo
+        secondPLabel.textColor = .systemIndigo
+        lLabel.textColor = .systemIndigo
+        eLabel.textColor = .systemIndigo
+        
+        indigoButtonToggled = true
+    }
+    
+    @IBAction func VioletButonTapped(_ sender: Any) {
+        aLabel.textColor = .purple
+        firstPLabel.textColor = .purple
+        secondPLabel.textColor = .purple
+        lLabel.textColor = .purple
+        eLabel.textColor = .purple
+        
+        violetButtonToggled = true
+    }
+    
+    @IBAction func blackButtonTapped(_ sender: Any) {
+        aLabel.textColor = .black
+        firstPLabel.textColor = .black
+        secondPLabel.textColor = .black
+        lLabel.textColor = .black
+        eLabel.textColor = .black
+        
+        blackButtonToggled = true
+    }
+    
 }
 
