a297deefcd02f42316241ac806c87e9cbd5743d2
Claudia Maciel <clc_80@yahoo.com>
2020-04-14 19:09:18 -0700

    Circle with plus sign

---
+++ logo/logo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  logo
+//
+//  Created by Claudia Contreras on 4/14/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
