84372baef3bfb6ea3ac2bc8048ffe5dbfd24eb96
Stephanie Ballard <user.name>
2020-05-13 13:30:17 -0700

    initial commit

---
+++ Logo-iOS17/Logo-iOS17/LogoView.swift
@@ -0,0 +1,22 @@
+//
+//  LogoView.swift
+//  Logo-iOS17
+//
+//  Created by Stephanie Ballard on 5/13/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LogoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Logo-iOS17/Logo-iOS17/LogoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LogoViewController.swift
+//  Logo-iOS17
+//
+//  Created by Stephanie Ballard on 5/13/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+
+    @IBOutlet weak var logoView: LogoView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
