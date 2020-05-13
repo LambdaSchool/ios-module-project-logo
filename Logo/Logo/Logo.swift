6b75f7d3a07ad2974b244867fc583b89c2b290dc
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-05-13 15:30:25 -0400

    logo

--- Logo/Logo/Logo.swift
+++ Logo/Logo/Logo.swift
@@ -13,13 +13,17 @@ import UIKit
 @IBDesignable
 class LogoView: UIView {
    
-    private let mouseColor = UIColor.black
-    private let bowColor = UIColor.red
+    private let bgColor = UIColor.systemIndigo.cgColor
+    private let whiteColor = UIColor.white.cgColor
+    private let startColor = UIColor.systemPurple.cgColor
+    private let endColor = UIColor.systemOrange.cgColor
+    
     
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.clear
+        
     }
     
     
@@ -33,78 +37,32 @@ class LogoView: UIView {
         
         if let context = UIGraphicsGetCurrentContext() {
             
-            let mouseCenter = CGPoint(x: rect.midX, y: rect.midY)
+            let center = CGPoint(x: rect.midX, y: rect.midY)
             
-            
-            //The Head
-            let headRadius: CGFloat = 65
-            let centerCircle = CGRect(x: mouseCenter.x - headRadius, y: mouseCenter.y - headRadius, width: 2 * headRadius, height: 2 * headRadius)
-            context.addEllipse(in: centerCircle)
-            context.setFillColor(mouseColor.cgColor)
-            context.fillPath()
-            
-            //The Left Ear
-            let leftEarRadius: CGFloat = 35
-            let leftCenterEar = CGRect(x: (mouseCenter.x - 120),
-                                       y: (mouseCenter.y - 105),
-                                       width: 2.5 * leftEarRadius,
-                                       height: 2.5 * leftEarRadius)
-            context.addEllipse(in: leftCenterEar)
-            context.setFillColor(mouseColor.cgColor)
-            context.fillPath()
-            
-            //The Right Ear
-            let rightEarRadius: CGFloat = 35
-            let rightCenterEar = CGRect(x: mouseCenter.x + 30,
-                                        y: mouseCenter.y - 105,
-                                        width: 2.5 * rightEarRadius,
-                                        height: 2.5 * rightEarRadius)
-            context.addEllipse(in: rightCenterEar)
-            context.setFillColor(mouseColor.cgColor)
+            let bgRect = CGRect(x: center.x - 100, y: center.y - 100, width: 200, height: 200)
+            let bgRectRounded = CGPath(roundedRect: bgRect, cornerWidth: 30, cornerHeight: 30, transform: nil)
+            context.addPath(bgRectRounded)
+            context.setFillColor(bgColor)
             context.fillPath()
             
             
-            // Middle of bow
             
-            let centerBowRadius: CGFloat = 25
-            let centerBowRect = CGRect(x: mouseCenter.x - 18,
-                                       y: mouseCenter.y - 85,
-                                       width: 10 + centerBowRadius,
-                                       height: 1.5 * centerBowRadius)
-
-            let centerBowRoundCourners = CGPath(roundedRect: centerBowRect,
-                                                cornerWidth: 5,
-                                                cornerHeight: 5,
-                                                transform: nil)
-            context.addPath(centerBowRoundCourners)
-            context.setFillColor(bowColor.cgColor)
+            let innerSquare = CGRect(x: center.x / 2.5, y: center.y / 2.5, width: bgRect.width - 20, height: bgRect.height - 20)
+            let innerSquareRounded = CGPath(roundedRect: innerSquare, cornerWidth: 20, cornerHeight: 20, transform: nil)
+            context.addPath(innerSquareRounded)
+            context.setStrokeColor(whiteColor)
+            context.setLineWidth(8)
+            context.strokePath()
+            
+            context.addEllipse(in: CGRect(x: center.x / 1.7, y: center.y / 1.7, width: rect.size.width / 2.5, height: rect.size.height / 2.5))
+            context.setStrokeColor(whiteColor)
+            context.setLineWidth(10)
+            context.strokePath()
+            
+            context.addEllipse(in: CGRect(x: center.x + 55, y: center.y - 70, width: rect.size.width / 15, height: rect.size.width / 15))
+            context.setFillColor(whiteColor)
             context.fillPath()
-            
-            // Top Left Bow
-            
-            let topLeftRadius: CGFloat = 40
-            let topLeftRect = CGRect(x: mouseCenter.x - 50,
-                                     y: mouseCenter.y - 110,
-                                     width: topLeftRadius * 1,
-                                     height: topLeftRadius * 2)
-//            context.translateBy(x: 10, y: -40)
-            //context.rotate(by: 25.06)
-            context.rotate(by: -25.19999)
-            context.addEllipse(in: topLeftRect)
-            context.setFillColor(bowColor.cgColor)
-            context.fillPath()
-            
-            // Top Right Bow
-            
-            let topRightRadius: CGFloat = 40
-            let topRightRect = CGRect(x: mouseCenter.x + 10,
-                                      y: mouseCenter.y - 132,
-                                      width: topRightRadius * 1,
-                                      height: topRightRadius * 2)
-           context.rotate(by: -37.55)
-            context.addEllipse(in: topRightRect)
-            context.setFillColor(bowColor.cgColor)
-            context.fillPath()
+           
         }
     }
 }
