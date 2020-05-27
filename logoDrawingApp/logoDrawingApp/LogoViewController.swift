cc5f686e2a27118a2b2970a230db2ddf5e99d56c
Clayton Watkins <watkinclayton17@yahoo.com>
2020-05-26 21:33:19 -0700

    Created first half of logo

---
+++ logoDrawingApp/logoDrawingApp/LogoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LogoViewController.swift
+//  logoDrawingApp
+//
+//  Created by Clayton Watkins on 5/26/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+
+    @IBOutlet weak var logoView: LogoView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
